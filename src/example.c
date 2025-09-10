cfd8d185f70e8eff0f5c3805a98e6fc5cace5dca
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-09-10 18:58:33 -0500

    feat: Add functions to handle IO to example program
    These functions will be useful to handle subprocesses later.

--- src/example.c
+++ src/example.c
@@ -1,11 +1,41 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #include "terminal.h"
 
 void write_chunk(Terminal *t, char *buf) {
   write_string(t, buf);
   print_terminal(t);
+}
+
+int setup_pipes(int stdin_pipe[2], int stdout_pipe[2]) {
+  if (pipe(stdin_pipe) == -1 || pipe(stdout_pipe) == -1) {
+    perror("pipe");
+    return 1;
+  }
+  return 0;
+}
+
+int execute_child_process(int stdin_pipe[2], int stdout_pipe[2]) {
+  close(stdin_pipe[1]);
+  close(stdout_pipe[0]);
+
+  dup2(stdin_pipe[0], STDIN_FILENO);
+  dup2(stdout_pipe[1], STDOUT_FILENO);
+
+  close(stdin_pipe[0]);
+  close(stdout_pipe[1]);
+
+  execl("/bin/bash", "/bin/bash", NULL);
+  perror("execl");
+  exit(1);
 }
 
 int main() {
