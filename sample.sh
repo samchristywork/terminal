add371f004a2704ec161c9eb5f6519cf06947586
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-11-13 19:54:59 -0600

    chore: Remove sample.sh
    This is an old file used for terminal testing. This will be replaced
    with more sophisticated testing later.

--- sample.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-printf '\033[31mRed Text\033[0m Normal Text\n'
-printf '\033[1;34mBold Blue\033[0m \033[42mGreen Background\033[0m\n'
-printf '\033[4;33mUnderlined Yellow\033[0m\n'
-printf '\033[7mReverse Video\033[0m\n'
-printf '\033[38;5;196m256-color Red\033[0m \033[48;5;46m256-color Green BG\033[0m\n'
-printf '\nTab Test:\tCol1\tCol2\tCol3\n'
-printf 'Line with \033[31mred\033[0m and \033[34mblue\033[0m words.\n'
